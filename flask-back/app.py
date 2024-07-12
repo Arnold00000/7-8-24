06e975224b942e27795783ca9a95745629a74508
Kimuli Arnold <kiflower35@gmail.com>
2024-07-12 12:28:00 -0700

    className instead of class

--- flask-back/app.py
+++ flask-back/app.py
@@ -2,9 +2,12 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+
+CORS(app)
 ma = Marshmallow(app)
 
 # Databse configuration                                  Username:password@hostname/databasename
--- react-front/src/pages/ListUserPage.js
+++ react-front/src/pages/ListUserPage.js
@@ -1,8 +1,23 @@
-import React, { } from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Link } from 'react-router-dom';
 
 export default function ListUserPage() {
+
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    function getUsers() {
+        axios.get('http://127.0.0.1:5000/listusers').then(function (response) {
+            console.log(response.data);
+            setUsers(response.data);
+        });
+    }
+
+
+
     return (
         <div>
             <div className="container h-100">
@@ -10,6 +25,20 @@ export default function ListUserPage() {
                     <div className="col-12">
                         <p> <Link to="/addnewuser" className="btn btn-success">Add New User</Link></p>
                         <h1>List Users</h1>
+                        <table className="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Date Added</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+
+
+
+                        </table>
                     </div>
                 </div>
             </div>
